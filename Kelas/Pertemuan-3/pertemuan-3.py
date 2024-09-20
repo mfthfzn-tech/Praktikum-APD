7eec7384dfe8304bac121ffb50007f9ce273121e
mfthfzn-tech <miftahulfauzan55@gmail.com>
2024-09-20 14:51:45 +0800

    Add files via upload

---
+++ Kelas/Pertemuan-3/pertemuan-3.py
@@ -0,0 +1,94 @@
+# cuaca = "cerah"
+
+# if cuaca == "cerah" :
+#     print("Kamu pergi keluar rumah")
+
+# # Variabel akan bernilai false jika string kosong, 0, [], (), {}, false
+# uang = True
+
+# uang = 1
+
+# if uang :
+#     print("Pergi ke Pasae!")
+
+# else :
+#     print("Pergi ke rumah teman")
+
+
+# nilai = 70
+
+# if nilai < 70 :
+#     print("Tidak Lulus")
+# else :
+#     print("Lulus")
+
+# nilai1 = 5
+# if nilai < 0 :
+#     print(f"{nilai1} adalah bilangan negaitf")
+# else :
+#     print(f"{nilai} adalah bilangan positif")
+
+# umur = int(input("Masukkan umur anda :"))
+
+# if umur <= 10 :
+#     print("Anak-anak")
+# elif umur <= 18 :
+#     print("Remaja")
+# elif umur <= 35 :
+#     print("Dewasa")
+# elif umur <= 65 :
+#     print("Parubaya")
+
+# nilai = 76
+
+# if nilai >= 80 :
+#     if nilai >= 85 :
+#         print("Nilai A+")
+#     else :
+#         print("Nilai A-")
+# else : 
+#     print("Tidak lulus")
+
+# print("Menu :")
+# print("1. Nonton film")
+# print("2. Ngoding")
+# print("3. Keluar")
+# pilihan = int(input("Masukkan pilihan :"))
+# if pilihan == 1 :
+#     print("Sedang nonton film")
+# elif pilihan == 2 :
+#     print("Sedang ngoding")
+# elif pilihan == 3 :
+#     print("Keluar dari aplikasi")
+# else :
+#     print("Menu tidak tersedia")
+
+# angka = 10 
+
+# # Pengecekan pertama
+# if angka % 2 == 0 :
+#     print("Genap")
+# if angka > 0 :
+#     print("Positif")
+
+# # Pengeckan kedua
+# if angka % 2 == 0 :
+#     print("Genap")
+# elif angka > 0 :
+#     print("Positif")
+
+
+# # Input jumlah barang
+# jumlah_barang = int(input("Masukkan jumlah brang :"))
+
+# # Input total harga
+# total_harga = int(input("Masukkan total harga :"))
+
+# # Pengecekan total harga dan total barang
+# if (jumlah_barang >= 5 and total_harga > 100.000) :
+#     total_harga = total_harga - (total_harga * 20/100)
+#     print(f"Anda membeli {jumlah_barang} barang dan total harga lebih dari 100.000 sehingga anda mendapatkan diskon sebesar 20%\n" + 
+#         f"total yang harus anda bayarkan adalah {total_harga}")
+# else :
+#     print(f"Anda membeli {jumlah_barang} barang, total harganya adalah {total_harga}")
+

626a760fae618c9df410685652b4ed7d0c55ef8f
mfthfzn-tech <miftahulfauzan55@gmail.com>
2024-09-27 15:00:49 +0800

    Add files via upload

---
+++ Kelas/Pertemuan-4/script.py
@@ -0,0 +1,81 @@
+# print("Perulangan ke-0")
+# print("Perulangan ke-1")
+# print("Perulangan ke-2")
+# print("Perulangan ke-3")
+# print("Perulangan ke-4")
+# print("Perulangan ke-5")
+# print("Perulangan ke-6")
+# print("Perulangan ke-7")
+# print("Perulangan ke-8")
+# print("Perulangan ke-9")
+
+# batas = 20
+# for i in range(10, batas, 2) :
+#     print(f"Perulangan ke-{i}")
+
+# buah = ["Apel", "Mangga", "Angggur"]
+# for i in buah :
+#     print(i)
+
+# for baris in range(1, 4) :
+#   for kolom in range(1, 4) :
+#       print(kolom, "kolom", end=" ")
+#   print()
+
+# while "" :
+#     print("Hello")
+
+# lagi = "yes"
+# ulang = 0
+# while lagi == "yes" :
+#     ulang += 1
+#     print("Mabar")
+#     lagi = input("Mabar lagi tidak")
+# print("Selesai Mabar")
+# print(f"Perulangan terjadi sebanyak {ulang}")
+
+# for i in range(1, 10) :
+#     if i == 4 :
+#         break
+#     else :
+#         print(f"perulangan ke-{i}")
+
+# buah_banyak = ["Apel", "Mangga", "Anggur"]
+# for buah in buah_banyak :
+#     if buah == "Anggur" :
+#         print("Buah anggur ketemu")
+#         break
+#     else :
+#         print("belum ketemu")
+
+# while True :
+#     ulang = input("Mabar lagi ?")
+#     if ulang == "gk" :
+#         break
+#     print("Mabar lagi")
+
+# for i in range(1, 10) :
+#     if i == 4:
+#         continue
+#     print(f"Perulangan ke-{i}")
+
+
+for i in range(5) :
+  for j in range(5) :
+    print("*", end= " ")
+  print()
+
+for j in range(1, 6) :
+  print("*" * j)
+
+
+
+
+
+
+
+
+
+
+
+
